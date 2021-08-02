ff1ae532db1f7e5dca53023428a93b1004e04918
Rob Weber <rweber@ecec.com>
2021-08-02 10:25:19 -0500

    removed this as now two different token methods

--- src/main/java/com/dustinredmond/sharepoint/SharePointAPI.java
+++ src/main/java/com/dustinredmond/sharepoint/SharePointAPI.java
@@ -24,17 +24,6 @@ public class SharePointAPI {
     }
     
     /**
-     * Provided as a convenience method, to avoid having to create
-     * a token via SharePointTokenFactory.getToken()
-     * @param username The SharePoint username e.g. person@example.com
-     * @param password The SharePoint user's password
-     * @param domain The subdomain of SharePoint
-     */
-    public SharePointAPI(String username, String password, String domain) {
-        this(TokenFactory.getUserToken(username, password, domain));
-    }
-    
-    /**
      * Executes a HTTP GET request at the given path.
      * https://youDomain.sharepoint.com/${path goes here}
      * @param path The API endpoint path
